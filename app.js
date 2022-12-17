e746339323cf9e221a0c7fb0e29448a19785e724
Vidhyadhar11 <94471463+Vidhyadhar11@users.noreply.github.com>
2022-12-17 22:49:07 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,16 @@
+const closedFace = document.querySelector('.closed');
+const openFace = document.querySelector('.open');
+
+closedFace.addEventListener('click', ()=>{
+    if(openFace.classList.contains('open')) {
+        openFace.classList.add('active');
+        closedFace.classList.remove('active');
+    }
+});
+openFace.addEventListener('click', ()=>{
+    if(closedFace.classList.contains('open')) {
+        closedFace.classList.add('active');
+        openFace.classList.remove('active');
+    }
+});
+ 
